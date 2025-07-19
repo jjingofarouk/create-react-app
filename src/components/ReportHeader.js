50c3a1f1e8d92ed1ccb91ee2a1db69a334a56876
jjingofarouk <jjingofaroukk@gmail.com>
2025-07-19 23:40:35 +0300

    Create ReportHeader.js

---
+++ src/components/ReportHeader.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ReportHeader = ({ title }) => {
+  return (
+    <h2 className="text-2xl font-bold text-neutral-800">{title}</h2>
+  );
+};
+
+export default ReportHeader;
