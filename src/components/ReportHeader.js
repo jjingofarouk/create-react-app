5f3b44ded5a03ea766b8b5a73d417db36c23df1a
jjingofarouk <jjingofaroukk@gmail.com>
2025-07-20 04:02:50 +0300

    Update ReportHeader.js

--- src/components/ReportHeader.js
+++ src/components/ReportHeader.js
@@ -2,9 +2,12 @@ import React from "react";
 
 const ReportHeader = ({ title }) => {
   return (
-    <h2 className="text-3xl font-extrabold text-gray-900 tracking-tight">
-      {title}
-    </h2>
+    <div className="flex items-center gap-4">
+      <img src="./logo.jpg" alt="Logo" className="w-12 h-12" />
+      <h2 className="text-3xl font-extrabold text-gray-900 tracking-tight">
+        {title}
+      </h2>
+    </div>
   );
 };
 
